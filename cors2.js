d81bb728b3a6a185b732bb2861fd1b4b59c8efde
Yaya7 <fufusimo@gmail.com>
2018-03-30 04:21:39 +0100

    Add files via upload

---
+++ cors2.js
@@ -0,0 +1,31 @@
+var express = require('express');  
+var request = require('request');
+
+var app = express();
+var gg = false;
+var gg2 = false;
+app.use('/', function(req, res, next) {
+	var url = req.url.substr(1);
+	req.headers = {};
+	req.headers['Host'] = "nba.com";
+	req.header['Cache-Control'] = 'no-cache';
+	req.header['User-Agent'] = 'iPad\r\n';
+	req.header['Accept'] = '*/*';
+	req.header['Accept-Encoding'] = 'gzip, deflate, sdch, br';
+	req.header['Accept-Language'] = 'en-US,en;q=0.8';
+	res.header('access-control-allow-origin', '*');
+	// console.log(url);
+	var proxy = request(url);
+	proxy.on('data', function(data) {
+		gg = data;
+		gg2 = url;
+		console.log('Proxy data: url: ' + url + "\n" + data); 
+	});
+	req.pipe(proxy);
+	proxy.pipe(res);
+});
+
+app.listen(process.env.PORT || 8080); 
+
+
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
